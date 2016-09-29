9b700b1585a6bb8fe207df5b8c71d8505ab92bba
mboger <marko.boger@gmail.com>
2016-09-29 18:58:01 +0200

    added JavaPerson

---
+++ de.htwg.scala.inAction/src/main/java/de/htwg/scala/person/JavaPerson.java
@@ -0,0 +1,31 @@
+package de.htwg.scala.person;
+
+public class JavaPerson {
+	
+	private int age;
+	private String name;
+	
+	public JavaPerson(String name, int age) {
+		this.name=name;
+		this.age=age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
